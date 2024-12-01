45f797ef6cd6df48e66b2157ee458d16fec397bb
TheDanishMexican <danieljchristjansen@gmail.com>
2024-12-01 13:44:11 +0100

    added closing tags inline if no children

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const htmlPage = document.documentElement;
+const selfClosingTags = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'source', 'track', 'wbr'];
 
 function printElementTagsWithIndentation(element, depth = 0) {
     const elementArr = Array.from(element.children);
@@ -7,12 +8,20 @@ function printElementTagsWithIndentation(element, depth = 0) {
         .map(attr => `${attr.name}="${attr.value}"`)
         .join(' ');
 
-    console.log(`${indentation}<${element.tagName.toLowerCase()}${attributes ? " " + attributes : ""}>`);
+    const openingTag = `<${element.tagName.toLowerCase()}${attributes ? " " + attributes : ""}>`;
 
+    if (elementArr.length === 0 && !selfClosingTags.includes(element.tagName.toLowerCase())) {
+        console.log(`${indentation}${openingTag}</${element.tagName.toLowerCase()}>`);
+    } else {
+        console.log(`${indentation}${openingTag}`);
 
-    elementArr.forEach(child => {
-        printElementTagsWithIndentation(child, depth + 1);
-    });
+        elementArr.forEach(child => {
+            printElementTagsWithIndentation(child, depth + 1);
+        });
+
+        if (!selfClosingTags.includes(element.tagName.toLowerCase())) {
+            console.log(`${indentation}</${element.tagName.toLowerCase()}>`);
+        }
+    }
 }
-
 
